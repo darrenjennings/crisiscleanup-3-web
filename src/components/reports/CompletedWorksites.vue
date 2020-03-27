f4c73deaa4ca377fb6939832806b725c8d0bab7f
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-27 00:59:02 -0500

    created components under "favorite" section

---
+++ src/components/reports/CompletedWorksites.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="flex-col m-5">
+      <div class="flex flex-row">
+        <base-text variant="h1">Completed Worksites</base-text>
+        <img src="@/assets/greylockss.jpg" class="h-8 px-3" />
+        <img src="@/assets/yellowstarss.jpg" class="ml-auto h-8" />
+      </div>
+      <base-text variant="body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sed cras ornare arcu
+        dui vivamus.
+      </base-text>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompletedWorksites',
+};
+</script>
