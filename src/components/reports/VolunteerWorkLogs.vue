2b60abd8f9cb0fd8c3831bdee30e5690523c336b
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-27 03:18:39 -0500

    fixed spacing & font weight of comps in "premium"

--- src/components/reports/VolunteerWorkLogs.vue
+++ src/components/reports/VolunteerWorkLogs.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <div class="flex-col m-5">
-      <div class="flex flex-row">
-        <base-text variant="h1"
-          >Detailed Work Logs with Volunteer Hours</base-text
-        >
-        <img src="@/assets/greylockss.jpg" class="h-8 px-3" />
-        <img src="@/assets/outlinestarss.jpg" class="ml-auto h-8" />
+    <div class="flex flex-col justify-around">
+      <div class="m-5">
+        <div class="flex flex-row">
+          <base-text variant="body" weight="700"
+            >Detailed Work Logs with Volunteer Hours</base-text
+          >
+          <img src="@/assets/greylockss.jpg" class="h-8 px-3" />
+          <img src="@/assets/outlinestarss.jpg" class="ml-auto h-8" />
+        </div>
+        <base-text variant="bodysm">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore
+        </base-text>
       </div>
-      <base-text variant="body">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </base-text>
     </div>
   </div>
 </template>
