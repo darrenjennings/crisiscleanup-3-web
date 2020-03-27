e406903992f3df8cbd0ad4d75b1d1a867bfb0220
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-27 01:07:25 -0500

    Created components in"free" section

---
+++ src/components/reports/LongestWaitTime.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="flex-col m-5">
+      <div class="flex flex-row">
+        <base-text variant="h1">Longest Wait Time to Completion</base-text>
+        <img src="@/assets/outlinestarss.jpg" class="ml-auto h-8" />
+      </div>
+      <base-text variant="body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </base-text>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LongestWaitTime',
+};
+</script>
