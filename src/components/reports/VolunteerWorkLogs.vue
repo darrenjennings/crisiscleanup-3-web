5576c50f92a3dd89ea6884bf4eda8be6195c3821
Vishnu Karthik <vishnu@arroyodev.com>
2020-03-27 01:09:02 -0500

    created components under "premium" section

---
+++ src/components/reports/VolunteerWorkLogs.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="flex-col m-5">
+      <div class="flex flex-row">
+        <base-text variant="h1"
+          >Detailed Work Logs with Volunteer Hours</base-text
+        >
+        <img src="@/assets/greylockss.jpg" class="h-8 px-3" />
+        <img src="@/assets/outlinestarss.jpg" class="ml-auto h-8" />
+      </div>
+      <base-text variant="body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </base-text>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VolunteerWorkLogs',
+};
+</script>
